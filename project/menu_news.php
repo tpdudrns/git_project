b85d7fd3862a4817540122f5bf72dd3346147b53
Ubuntu <ubuntu@ip-172-31-39-91.ap-northeast-2.compute.internal>
2020-07-27 15:37:01 +0000

    modified / 뉴스 페이지 / css 파일 연결 경로 수정 (이유: 서버 재설치)

--- project/menu_news.php
+++ project/menu_news.php
@@ -2,7 +2,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <title>Welcome, 메인 페이지</title>
-<link rel="stylesheet" type="text/css" href="/git_project/project/css/style_home.css">
+<link rel="stylesheet" type="text/css" href="/project/css/style_home.css">
 </head>
 <style>
 
