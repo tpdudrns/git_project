8328ef4502f01943f7be037eb1cd35cd52945930
Ubuntu <ubuntu@ip-172-31-39-91.ap-northeast-2.compute.internal>
2020-07-27 15:31:10 +0000

    add / main페이지 / 서버 재설치하면서 새로 생성

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Welcome, 메인 페이지</title>
+<link rel="stylesheet" type="text/css" href="/project/css/style_home.css">
+</head>
+<body>
+  <div class = "wrap">
+    <header>
+      <div id="login_area">
+        <ul>
+        <li><a href = "/git_project/project/page/product_board/product_cart.php">장바구니 / </a?</li>
+          <?php
+            session_start();
+            if(!isset($_SESSION['userid'])) {
+              echo "<li><a href = \"/project/test_login.php\">로그인</a></li>";
+            } else {
+              $id = $_SESSION['userid'];
+              echo "<li>$id 님 환영합니다. / </a></li>";
+              echo "<li><a href = \"/logout_action.php\">로그아웃</a></li>";
+            }
+          ?>
+          <!-- <li><a href = "test_login.php">로그인</a></li> -->
+        </ul>
+      </div>
+      <div id="title">
+        <h1>SY's Interior Story</h1> 
+      </div>
+    </header>
+    <nav>
+      <ul>
+        <li><a href = "/">홈</a></li>
+        <li><a href = "/project/menu_news.php">인테리어 소식</a></li>
+        <li><a href = "/project/menu_album.php">앨범</a></li>
+        <li><a href = "/project/menu_product_list.php">소품</a></li>
+        <li><a href = "/project/menu_board.php">게시판</a></li>
+      </ul>
+    </nav>
+
+   <article>
+    <div class="main_image">
+      <img src="/project/images/main_pic.jpg" alt="" />
+    </div>
+   </article>
+    <footer>
+      ::: Contact : sinsy@gmail.com :::
+    </footer>
+  </div>
+<script type="text/javascript"> 
+  function getCookie(name) {
+     var cookie = document.cookie; 
+     if (document.cookie != "") { 
+       var cookie_array = cookie.split("; ");
+        for ( var index in cookie_array) { 
+          var cookie_name = cookie_array[index].split("=");
+           if (cookie_name[0] == "popupYN") {
+              return cookie_name[1];
+               }
+        } 
+      } return ;
+  } 
+                 
+  function openPopup(url) {
+      var cookieCheck = getCookie("popupYN");
+      if (cookieCheck != "N") window.open(url, '', 'width=450,height=750,left=0,top=0')
+  } 
+</script>
+<body onload="javascript:openPopup('popup.html')">
+
+</body>
+</html>
+
