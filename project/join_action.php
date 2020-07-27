d08e58bcdfa4d66e384c34a6a9f5b373a526bf74
Ubuntu <ubuntu@ip-172-31-39-91.ap-northeast-2.compute.internal>
2020-07-27 14:15:46 +0000

    modified / 회원가입 php 페이지 / 이메일 항목 추가

--- project/join_action.php
+++ project/join_action.php
@@ -4,7 +4,7 @@ ini_set("display_errors", 1);
 
 
 //include  $_SERVER['DOCUMENT_ROOT']."/git_proejct/db_info.php";
-include "db_info.php";
+include  $_SERVER['DOCUMENT_ROOT']."/db.php";
 
 function Console_log($data){
     echo "<script>console.log( 'PHP_Console: " . $data . "' );</script>";
@@ -15,11 +15,11 @@ Console_log($testVal);
     $id=$_POST['id'];
     $pw=$_POST['pw'];
     $hashed_password = password_hash($pw, PASSWORD_DEFAULT);
-    $nickname=$_POST['nickname'];
-    $date = date('Y-m-d H:i:s');
+    $email=$_POST['email'];
+    
  
     //입력받은 데이터를 DB에 저장
-    $query = "insert into member (idx, id, password, nickname, date) values (null, '$id', '$hashed_password', '$nickname', '$date')";
+    $query = "insert into member (idx, id, password, email) values (null, '$id', '$hashed_password', '$email')";
     $result = $connect->query($query);
 
     
