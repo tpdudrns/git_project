b691b49b84c8ca12b13f5099d6f352a8a7037465
Ubuntu <ubuntu@ip-172-31-39-91.ap-northeast-2.compute.internal>
2020-07-27 15:40:44 +0000

    modified / 게시글 삭제 / db 연결 파일 경로 재설정 (서버 재설치로 인해서)

--- project/page/board/delete.php
+++ project/page/board/delete.php
@@ -1,8 +1,8 @@
 <?php
-//	include "/git_proejct/db_connection.php";
-	include "db_connection.php";	
+	include $_SERVER['DOCUMENT_ROOT']."/db_connection.php"; /* db load */
+	session_start();
 	$get_index = $_GET['idx'];
 	$sql = mq("delete from freeboard where idx='$get_index';");
 ?>
 <script type="text/javascript">alert("삭제되었습니다.");</script>
-<meta http-equiv="refresh" content="0 url=https://interiorsy.tk/git_project/project/menu_board.php" />
+<meta http-equiv="refresh" content="0 url=/project/menu_board.php" />
