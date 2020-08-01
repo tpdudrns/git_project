f3bff31069d42c4891cbf7292466a658d1b06460
Ubuntu <ubuntu@ip-172-31-39-91.ap-northeast-2.compute.internal>
2020-08-01 11:15:04 +0000

    modified / 장바구니 페이지 / css 파일 연결 경로 수정 (사유: 서버 재설치)

--- project/page/product_board/product_cart.php
+++ project/page/product_board/product_cart.php
@@ -56,7 +56,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <title>Welcome, 메인 페이지</title>
-<link rel="stylesheet" type="text/css" href="/git_project/project/css/style_home.css">
+<link rel="stylesheet" type="text/css" href="/project/css/style_home.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" /> -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
