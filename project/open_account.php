078b08541557ee963dd435072173fe0d8d48f46a
Ubuntu <ubuntu@ip-172-31-39-91.ap-northeast-2.compute.internal>
2020-08-01 11:18:59 +0000

    modified / 회원가입 페이지 / id 중복체크 기능 추가

--- project/open_account.php
+++ project/open_account.php
@@ -1,36 +1,71 @@
+<?php
+ include  $_SERVER['DOCUMENT_ROOT']."/db_connection.php";
+?>
 <!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <title>회원가입</title>
+  <script type="text/javascript" src="/project/js/jquery-3.5.1.min.js"></script>
 </head>
+<script>
+function checkid(){
+	var userid = document.getElementById("userid").value;
+	if(userid)
+	{
+		url = "/check.php?userid="+userid;
+			window.open(url,"chkid","width=300,height=100");
+		}else{
+			alert("아이디를 입력하세요");
+		}
+	}
+</script>
 <style>
+    body {
+    background-image:url('/uploads/blue.jpg');
+    background-size: 750px 800px;
+    background-repeat: no-repeat;
+    background-position: center top;
+    background-color: black;
+  }
   ul {padding: 0;}
   li {list-style:none;}
   .login { width: 410px; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);}
-  .login h4 { padding: 0 0 10px; font-size: 25px; color: #111; border-bottom: 2px solid #000;
+  .login h4 { padding: 0 0 10px; font-size: 25px; color: white; border-bottom: 2px solid white;
   text-align: center;}
-  .login li { padding: 0 0 12px;}
+  .login li { color: white; padding: 0 0 12px;}
   .login li input { width: 100%; height: 46px; box-sizing: border-box; }
-  .login button { border: 0; outline: 0; width: 100%; height: 56px; box-sizing: border-box; background: red; color: white; font-size: 18px;}
+  .login button { border: 0; outline: 0; width: 100%; height: 56px; box-sizing: border-box; background: black; color: white; font-size: 18px;}
+  #userid {
+    width: 310px;
+  }
+  #id_double_check {
+    width: 90px; background: black; color: white; border: 0; outline: 0;
+  }
+  #id_area {
+    float: left;
+  }
 </style>
 <body>
   <form action="join_action.php" method="post">
   <section class="login">
-    <h4>회원가입</h4>
+    <h4>Sign-in</h4>
     <ul>
       <li>사용자 아이디</li>
-      <li><input type="text" name= "id" placeholder="아이디" title="아이디 입력"></li>
+      <li id=id_area>
+      <input type="text" name= "id" placeholder="아이디" title="아이디 입력" id="userid">
+      <input type=button value="중복검사" onclick="checkid();" id=id_double_check>
+      <input type="hidden" value="0" name="chs" />
+      </li>
       <li>비밀번호</li>
       <li><input type="password" name= "pw" placeholder="비밀번호" title="비밀번호 입력"></li>
       <li>비밀번호 확인</li>
       <li><input type="password" name= "pw" placeholder="비밀번호" title="비밀번호 입력"></li>
       <li>이메일</li>
       <li><input type="text" name= "email" placeholder="이메알 주소" title="닉네임 입력"></li>
-      <li><input type="submit" value="Join"></li>
+      <li><button type="submit">Join</button></li>
     </ul>
   </section>
   </form>
-
 
 </body>
 </html>
