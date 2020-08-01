01f20420bdfda358d084fd9adab908a04c6d5d45
Ubuntu <ubuntu@ip-172-31-39-91.ap-northeast-2.compute.internal>
2020-08-01 11:18:15 +0000

    modified / 로그인 페이지 / 배경화면 추가

--- project/test_login.php
+++ project/test_login.php
@@ -4,24 +4,37 @@
   <title>로그인</title>
 </head>
 <style>
+  body {
+    background-image:url('/uploads/blue.jpg');
+    background-size: 750px 800px;
+    background-repeat: no-repeat;
+    background-position: center top;
+    background-color: black;
+  }
   ul {padding: 0;}
   li {list-style:none;}
   .login { width: 410px; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);}
-  .login h2 { padding: 0 0 10px; font-size: 32px; color: #111; border-bottom: 2px solid #000;
+  .login h2 { padding: 0 0 10px; font-size: 32px; color: white; border-bottom: 2px solid white;
   text-align: center;}
   .login li { padding: 0 0 12px;}
   .login li input { width: 100%; height: 46px; box-sizing: border-box; }
-  .login button { border: 0; outline: 0; width: 100%; height: 56px; box-sizing: border-box; background: red; color: white; font-size: 18px;}
-
+  .login button { border: 0; outline: 0; width: 100%; height: 56px; box-sizing: border-box; background: black; color: white; font-size: 18px;}
+  div {
+    text-align: center;
+    color: white;
+  }
+  a {
+    color: white;
+  }
 </style>
 <body>
   <form action="login_action.php" method="post">
   <section class="login">
-    <h2>로그인</h2>
+    <h2>Log-in</h2>
     <ul>
       <li><input type="text" name= "id" placeholder="아이디" title="아이디 입력"></li>
       <li><input type="password" name= "pw" placeholder="비밀번호" title="비밀번호 입력"></li>
-      <li><input type="submit" value="로그인"></li>
+      <li><button type="submit" value="로그인">로그인</button></li>
     </ul>
     <div>
       <a href= "open_account.php">"회원가입"</a>
