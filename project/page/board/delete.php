446fb84320a06e1663ebae395d569c51a008bf95
Ubuntu <ubuntu@ip-172-31-39-91.ap-northeast-2.compute.internal>
2020-08-02 06:24:09 +0000

    modified/ 게시글 삭제 페이지 / id 와 작성자가 같을 때에만 삭제되도록 수정

--- project/page/board/delete.php
+++ project/page/board/delete.php
@@ -1,8 +1,22 @@
 <?php
 	include $_SERVER['DOCUMENT_ROOT']."/db_connection.php"; /* db load */
 	session_start();
+
+	$URL = "/project/menu_board.php";
 	$get_index = $_GET['idx'];
-	$sql = mq("delete from freeboard where idx='$get_index';");
+	$sql = mq("select * from freeboard where idx='$get_index';");
+	$board = $sql->fetch_array();
+
+	if(!isset($_SESSION['userid'])) {
+	?> <script>
+			alert("권한이 없습니다.");
+			location.replace("<?php echo $URL?>");
+		</script>
+	<?php
+	} else if ($_SESSION['userid']==$board['name']) {
+		$sql = mq("delete from freeboard where idx='$get_index';"); ?>
+		<script type="text/javascript">alert("삭제되었습니다.");</script>
+	<?php
+	}
 ?>
-<script type="text/javascript">alert("삭제되었습니다.");</script>
 <meta http-equiv="refresh" content="0 url=/project/menu_board.php" />
